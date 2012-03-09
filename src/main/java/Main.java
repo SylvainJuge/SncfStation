ccd2b976e7446576c959cba3be91cf21dcceedf8
Sylvain Juge <sylvain.juge@vidal.fr>
2012-03-09 16:00:46 +0100

    first working draft
    - html entity escaping thanks to commons-lang
    - add a TODO list

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,49 @@
 import java.io.InputStream;
 import java.io.IOException;
+import java.io.StringReader;
+import java.io.ByteArrayInputStream;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.HttpEntity;
+import org.apache.http.Header;
+import org.apache.http.util.EntityUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import org.apache.commons.lang.StringEscapeUtils;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.XMLEvent;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+
 public class Main {
-    // -- features
-    // TODO : parse whole table and display it as ascii
-    // -- enhancements
-    // TODO : allow for another station
-    // TODO : allow for departure and arrivals
-    // TODO : do a statistical analysis of which train arrives at which platform
+    private static final XMLInputFactory XML_INPUT_FACTORY = XMLInputFactory.newInstance();
+    static {
+        XML_INPUT_FACTORY.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES,false);
+        XML_INPUT_FACTORY.setProperty(XMLInputFactory.IS_VALIDATING,false);
+        XML_INPUT_FACTORY.setProperty(XMLInputFactory.SUPPORT_DTD,false);
+        XML_INPUT_FACTORY.setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES,true);
+    }
+
     private static final String URL = "http://www.gares-en-mouvement.com/fr/frpno/horaires-temps-reel/dep/";
 
     public static void main(String[] args){
         HttpClient client = new DefaultHttpClient();
         try {
-            print(doRequest(client, URL));
+            printXml(doRequest(client, URL));
         } finally {
             client.getConnectionManager().shutdown();
         }
     }
 
-
-    private static InputStream doRequest(HttpClient client, String url) {
+    private static String doRequest(HttpClient client, String url) {
         HttpGet httpGet = new HttpGet(URL);
         try {
             HttpResponse response = client.execute(httpGet);
@@ -34,15 +51,81 @@ public class Main {
             if( null == entity ){
                 throw new RuntimeException("no entity in response");
             }
-            return entity.getContent();
+            Header encodingHeader = entity.getContentEncoding();
+            String encoding = null != encodingHeader ? encodingHeader.getValue().toUpperCase() : "UTF-8";
+            return EntityUtils.toString(entity,encoding);
         } catch (IOException e){
             throw new RuntimeException(e);
         }
-        // TODO : detect encoding from response
     }
 
-    private static void print(InputStream input){
+    private static void printXml(String xml){
+        String unescapedHtml = StringEscapeUtils.unescapeHtml(xml);
+        // we have to remove '&' chars from stream for easy parsing (not relevant in terms of "data")
+        unescapedHtml = unescapedHtml.replaceAll("&",""); 
+        InputStream input = new ByteArrayInputStream(unescapedHtml.getBytes());
 
+        boolean inTable = false;
+        boolean inRow = false;
+        String cellType = null;
+
+        try {
+            XMLEventReader reader = XML_INPUT_FACTORY.createXMLEventReader(input);
+            while(reader.hasNext()){
+                XMLEvent event = reader.nextEvent();
+                if(event.isStartElement()){
+                    StartElement start = event.asStartElement();
+                    if(tagMatch(start,"table","tab_horaires_tps_reel")){
+                        inTable = true;
+                    } else if(inTable && tagMatch(start,"tr")){
+                        inRow = true;
+                    } else if( inRow && tagMatch(start,"td")){
+                        cellType = getTagClass(start);
+                    }
+                }
+                if(event.isCharacters() && null != cellType){
+                    Characters text = event.asCharacters();
+                    String data = text.getData().trim();
+                    if(!"h".equals(data)){
+                        System.out.println(cellType + " " + text.getData());
+                    }
+                }
+                if(event.isEndElement()){
+                    EndElement end = event.asEndElement();
+                    if(tagMatch(end,"table")){
+                        inTable = false;
+                    } else if( inTable && tagMatch(end,"tr")){
+                        inRow = false;
+                    } else if( inRow && tagMatch(end,"td")){
+                        cellType = null;
+                    }
+                }
+            }
+        } catch( XMLStreamException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static boolean tagMatch(EndElement end, String tag){
+        return tag.equals(end.getName().getLocalPart());
+    }
+
+    private static boolean tagMatch(StartElement start, String tag){
+        return tag.equals(start.getName().getLocalPart());
+    }
+
+    private static boolean tagMatch(StartElement start, String tag, String classAttribute){
+        String clazz = getTagClass(start);
+        return tagMatch(start,tag) && classAttribute.equals(clazz);
+    }
+
+    private static String getTagClass(StartElement start){
+        Attribute clazz = start.getAttributeByName(new QName("class"));
+        return null != clazz ? clazz.getValue() : null;
+    }
+
+    // first dummy implementation
+    private static void print(InputStream input){
         int read = 0;
         do {
             try {
