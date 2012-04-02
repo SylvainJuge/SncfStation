1c6ac4b4053269c46448130847bf281ba6eb60e4
Sylvain Juge <syl20j@gmail.com>
2012-04-02 08:56:51 +0200

    API documentation & formatting

--- src/main/java/sylvain/juge/sncfstation/AsciiTable.java
+++ src/main/java/sylvain/juge/sncfstation/AsciiTable.java
@@ -4,13 +4,24 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class AsciiTable {
-    // characters used to decorate ascii table
+
+    /** character used for column separation */
     private final char columnSeparator;
+
+    /** character used for row separation, and horizontal separtors */
     private final char rowSeparator;
+
+    /** character used for corner and junctions */
     private final char corner;
 
+    /** list of data rows */
     private final List<List<String>> rows;
+
+    /** column widths, holds maximum column width for each data columns */
     private final List<Integer> columnWidths;
+
+    /** reference used as marker for separator rows */
+    private final static List<String> SEPARATOR = new ArrayList<String>();
 
     // TODO : enhance this class and create a builder to make it immutable ??
     
@@ -22,18 +33,24 @@ public class AsciiTable {
         this.columnWidths = new ArrayList<Integer>();
     }
 
-    /** @return ascii table with default separators */
+    /**
+     * @return ascii table with default separators 
+     */
     public static AsciiTable newDefault(){
         return new AsciiTable('|','-','o');
     }
 
-    // TODO : separator rows must be implemented using a reference to a static 
-    // value in order to avoid re-formatting them when a column is resized
+    /** 
+     * Adds a separator row to table 
+     */
     public void addSeparatorRow(){
         rows.add(SEPARATOR);
     }
-    private final static List<String> SEPARATOR = new ArrayList<String>();
 
+    /** 
+     * Adds a data row to table
+     * @param row : row to add
+     */
     public void addRow(List<String> row){
         // we have to copy values when thy are added because
         // lists are mutable by nature
@@ -50,23 +67,29 @@ public class AsciiTable {
         }
    }
 
-    public int getWidth(){
-        // columns count -1 + 2 separators
-        // + sum of all columns widths
-        int width = columnWidths.size()+1;
-        if( width < 2 ){
-            width = 2;
-        }
-        for(int columnWidth:columnWidths){
-            width+=columnWidth;
-        }
-        return width;
-    }
+   /** @return table total width */
+   public int getWidth(){
+       // columns count -1 + 2 separators
+       // + sum of all columns widths
+       int width = columnWidths.size()+1;
+       if( width < 2 ){
+           width = 2;
+       }
+       for(int columnWidth:columnWidths){
+           width+=columnWidth;
+       }
+       return width;
+   }
 
+    /** @return table total height */
     public int getHeight(){
         return 2 + rows.size();
     }
 
+    /** 
+     * Renders table rows formatted for printing
+     * @return formatted table rows 
+     */
     public List<String> getRows(){
         List<String> result = new ArrayList<String>();
         result.add(separatorRow());
@@ -122,9 +145,9 @@ public class AsciiTable {
         return sb.toString();
     }
 
-    @Override
-    public String toString(){
-        return null;
-    }
+    //@Override
+    //public String toString(){
+    //    return null;
+    //}
 }
 
